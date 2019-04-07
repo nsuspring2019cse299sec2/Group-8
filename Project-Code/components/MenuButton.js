a7a3202c93a952bba6fa3d09d2a46e461da5f9d4
uchchwas1 <47281896+uchchwas1@users.noreply.github.com>
2019-04-07 02:31:20 -0800

    modify menuButton for better look

--- Project-Code/components/MenuButton.js
+++ Project-Code/components/MenuButton.js
@@ -8,7 +8,7 @@ export default class MenuButton extends React.Component {
 			<Ionicons
 				name="md-menu"
 				color="#000000"
-				size={32}
+				size={20}
 				style={styles.menuIcon}
 				onPress={() => this.props.navigation.toggleDrawer()}
 			/>
@@ -20,7 +20,7 @@ const styles = StyleSheet.create({
 	menuIcon: {
 		zIndex: 9,
 		position: 'absolute',
-		top: 40,
-		left: 20,
+		top: 20,
+		left: 10,
 	}
 })
